949fe77ea514735ea8f59d30f875dc767c326c4d
beautifuldonkey <beautifuldonkey949@gmail.com>
2016-07-27 06:38:51 -0700

    TodoActivity.java -binding click event for note selection --displaying toast -binding click event for add item button --displaying popup and binding button click events to close popup

--- app/src/main/java/beautifuldonkey/beautifultodo/TodoActivity.java
+++ app/src/main/java/beautifuldonkey/beautifultodo/TodoActivity.java
@@ -4,10 +4,16 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ListViewCompat;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import beautifuldonkey.beautifultodo.adapters.AdapterManager;
 import beautifuldonkey.beautifultodo.data.Note;
@@ -16,6 +22,7 @@ import beautifuldonkey.beautifultodo.data.TodoConstants;
 
 public class TodoActivity extends AppCompatActivity {
 
+  Button btnAddItem;
   Context context;
 
   @Override
@@ -27,12 +34,52 @@ public class TodoActivity extends AppCompatActivity {
     NoteList todoList = getIntent().getParcelableExtra(TodoConstants.INTENT_EXTRA_LIST);
 
     TextView textListName = (TextView) findViewById(R.id.text_list_name);
-    textListName.setText(todoList.getName());
+    if(textListName!=null){
+      textListName.setText(todoList.getName());
+    }
 
     ListView listTodoItems = (ListView) findViewById(R.id.list_todo_items);
     ArrayAdapter<Note> adapterTodo = AdapterManager.getNoteAdapter(context,todoList.getNotes());
-    listTodoItems.setAdapter(adapterTodo);
+    if(listTodoItems!=null){
+      listTodoItems.setAdapter(adapterTodo);
+      listTodoItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+          Toast.makeText(context,"note selected",Toast.LENGTH_SHORT).show();
+        }
+      });
+    }
 
-    Button btnAddItem = (Button) findViewById(R.id.btn_add_item);
+    btnAddItem = (Button) findViewById(R.id.btn_add_item);
+    if(btnAddItem!=null){
+      btnAddItem.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+          LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+          View view = inflater.inflate(R.layout.popup_new_item,null);
+          final PopupWindow popupWindow = new PopupWindow(view,300,500,true);
+          popupWindow.setContentView(view);
+          popupWindow.showAtLocation(btnAddItem, Gravity.CENTER,0,0);
+
+          Button btnCancel = (Button) view.findViewById(R.id.btn_cancel);
+          btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              popupWindow.dismiss();
+            }
+          });
+
+          Button btnDone = (Button) view.findViewById(R.id.btn_done);
+          btnDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              popupWindow.dismiss();
+            }
+          });
+
+        }
+      });
+    }
+
   }
 }
