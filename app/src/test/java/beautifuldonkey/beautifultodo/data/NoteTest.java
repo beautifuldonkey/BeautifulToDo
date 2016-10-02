e60b9b927f83c829db11e42dfa422a8508294d34
beautifuldonkey <beautifuldonkey949@gmail.com>
2016-10-01 23:48:22 -0700

    Note.java -removed unnecessary white space
    NoteTest.java
    -test suite for Note class

---
+++ app/src/test/java/beautifuldonkey/beautifultodo/data/NoteTest.java
@@ -0,0 +1,60 @@
+package beautifuldonkey.beautifultodo.data;
+
+import android.os.Parcel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static junit.framework.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Unit test for the Note class
+ * Created by jaw_m on 10/1/2016.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class NoteTest {
+
+  @Mock
+  Parcel parcel;
+
+  public NoteTest(){
+    super();
+  }
+
+  Note testNote = new Note();
+
+  String testNoteName = "testName";
+  String testNoteComments = "testComments";
+
+  @Test
+  public void testBuildNote(){
+    testNote.setName(testNoteName);
+    testNote.setComments(testNoteComments);
+    assertThat(testNote.getName(),is(testNoteName));
+    assertThat(testNote.getComments(),is(testNoteComments));
+  }
+
+  @Test
+  public void testNoteParcelable(){
+    testNote.writeToParcel(parcel,0);
+    parcel.setDataPosition(0);
+    Note newTestNote = Note.CREATOR.createFromParcel(parcel);
+    assertNotNull(newTestNote);
+  }
+
+  @Test
+
+  public void testNoteParcelableArray(){
+    Note[] testNoteArray = Note.CREATOR.newArray(1);
+    assertNotNull(testNoteArray);
+  }
+
+  @Test
+  public void testNoteContents(){
+    assertThat(testNote.describeContents(),is(0));
+  }
+}
