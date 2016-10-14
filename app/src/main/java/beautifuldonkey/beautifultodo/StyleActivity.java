89c79f0c8b191667b4805f0b986c242a231ddef4
beautifuldonkey <beautifuldonkey949@gmail.com>
2016-10-13 19:30:22 -0700

    StyleActivity.java -displaying default text size value and allowing user to customize --only impact current activity, will expand to rest of application
    activity_style.xml
    -adding layout elements to display text size and allow user to make updates
    
    dimens.xml
    -adding resource element for application text size
    
    strings.xml
    -adding resource elements for new layout elements

--- app/src/main/java/beautifuldonkey/beautifultodo/StyleActivity.java
+++ app/src/main/java/beautifuldonkey/beautifultodo/StyleActivity.java
@@ -2,12 +2,61 @@ package beautifuldonkey.beautifultodo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class StyleActivity extends AppCompatActivity {
+
+  int textSize;
+
+  TextView tvTextSize;
+  TextView tvTextSizeLabel;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_style);
+
+    TypedValue typedValue = new TypedValue();
+    this.getResources().getValue(R.integer.text_size,typedValue,true);
+    textSize = typedValue.data;
+
+    tvTextSizeLabel = (TextView) findViewById(R.id.text_size_label);
+    tvTextSize = (TextView) findViewById(R.id.text_size);
+    if(tvTextSize != null && tvTextSizeLabel != null){
+      tvTextSize.setText(String.valueOf(textSize));
+      tvTextSize.setTextSize(textSize);
+      tvTextSizeLabel.setTextSize(textSize);
+    }
+
+    Button btnAddTextSize = (Button) findViewById(R.id.btn_add);
+    if(btnAddTextSize!=null){
+      btnAddTextSize.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+          textSize += 1;
+          updateStyle();
+        }
+      });
+    }
+
+    Button btnSubTextSize = (Button) findViewById(R.id.btn_subtract);
+    if(btnSubTextSize != null){
+      btnSubTextSize.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+          textSize -= 1;
+          updateStyle();
+        }
+      });
+    }
+  }
+
+  public void updateStyle(){
+    tvTextSize.setText(String.valueOf(textSize));
+    tvTextSize.setTextSize(textSize);
+    tvTextSizeLabel.setTextSize(textSize);
   }
 }
